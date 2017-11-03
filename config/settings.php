fd155f4214ea1b5cc3daadeb02486d7319b748d2
Vo Tuan Thanh <thanh.dtu94@gmail.com>
2017-11-03 16:29:10 +0700

    update link more tool

--- config/settings.php
+++ config/settings.php
@@ -194,6 +194,6 @@ return [
 
     'hour_default' => '00:00',
     'other_link' => [
-        'more_tool' => 'http://education.framgia.vn/tools',
+        'more_tool' => 'http://wsm.framgia.vn/all-tools',
     ],
 ];
